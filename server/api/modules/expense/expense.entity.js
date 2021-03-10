b9980f6a3614326e7452a13c6800d44eca183dc1
Laribok.Syiemlieh <laribok.syiemlieh@ibm.com>
2021-03-10 23:15:29 +0530

    create expense entity

---
+++ server/api/modules/expense/expense.entity.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ExpenseSchema = new Schema({
+  id: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  title: {
+    type: String,
+    lowecase: true,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: String,
+    required: true,
+  },
+  expenseDate: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("expense", ExpenseSchema);
